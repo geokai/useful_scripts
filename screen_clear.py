ab1016e8ca06003656929b2e64834f72bd316a10
George Kaimakis <geomatus@mac.com>
2017-07-08 23:56:53 +0100

    Adds screen_clear.py file:

---
+++ screen_clear.py
@@ -0,0 +1,22 @@
+# This file was created on 07/07/2017
+# Author: George Kaimakis
+
+
+# this module determines the operating system type and provides the
+# appropriate terminal scroll-back clearing (screen clear):
+
+import os
+
+def clear():
+    osname = os.name
+    if osname == 'posix':
+        os.system('clear')
+    elif osname == 'nt' or osname == 'dos':
+        os.system('cls')
+    else:
+        print("\n" * 30)
+    return osname
+
+if __name__ == "__main__":
+    clear()
+
