9fcb1e991a0c92e01d8864222594853f5d9f8a1a
George Kaimakis <geomatus@mac.com>
2017-07-24 13:35:42 +0100

    Syntastic errors corrected:
    10/12 syntastic errors corrected,
    • no doc-strings - module and function
    • out of sequence imports
    • unacceptable constant names
    • misc erronous spaces
    • empty EOF line
    • superfoulous parens at 'while' call
    
    2 'unacceptable variable names' remain

--- cpu_mem.py
+++ cpu_mem.py
@@ -1,42 +1,41 @@
+"""Checks the processor and memory useage percentage using the psutil tool."""
+
 # This file was created on 07/07/17
 # Author: George Kaimakis
 
-# this script checks the processor and memory (ram) use percentage using the
-# psutil tool.
 
 from __future__ import print_function
-import psutil
 from time import sleep
-from screen_clear import clear
 import sys
+import psutil
+from screen_clear import clear
 
 clear()
-counter = 0
 
 def buf_flush():
+    """Flushes the stdout buffer."""
     sys.stdout.flush()
 
-duration = int(input("Select an interval from 1 to 30 seconds: "))
+COUNTER = 0
+DURATION = int(input("Select an interval from 1 to 30 seconds: "))
 clear()
-
-while(True):
-    if duration > 0 and duration < 31:
+while True:
+    if DURATION > 0 and DURATION < 31:
         # get the system performance data:
-        cpuPercent = psutil.cpu_percent(interval=duration)
+        cpuPercent = psutil.cpu_percent(interval=DURATION)
         ramPercent = psutil.virtual_memory().percent
         print()
-        print("{0}/{1}: ".format(duration, (counter + 1)), end="")
+        print("{0}/{1}: ".format(DURATION, (COUNTER + 1)), end="")
         buf_flush()
         sleep(0.5)
-        print ("CPU = {0}%  RAM = {1}% ".format(cpuPercent, ramPercent), end='')
+        print("CPU = {0}%  RAM = {1}% ".format(cpuPercent, ramPercent), end='')
         buf_flush()
         sleep(0.5)
-        counter += 1
-        if counter == duration:
-            counter = 0
+        COUNTER += 1
+        if COUNTER == DURATION:
+            COUNTER = 0
     else:
         print("select only 1 to 30 seconds!")
         sleep(2)
         clear()
         break
-
