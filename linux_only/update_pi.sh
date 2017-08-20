fa01a35871b8cd35a811c5bde661a53111ba06e5
George Kaimakis <geomatus@mac.com>
2017-08-20 12:54:03 +0100

    Adds the 'upgrade' function and visual indication:

--- linux_only/update_pi.sh
+++ linux_only/update_pi.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-"""this script updates the raspbian repos and then upgrades the system"""
+
+# this script updates the raspbian repos and then upgrades the system:
 
 # This file was created on 20/08/2017
 # Author: George Kaimakis - https://github.com/geokai
+
+echo 'updating the raspbian reopos...'
 sudo apt-get update -y
+
+echo 'now upgrading the system...'
+sudo apt-get upgrade -y
