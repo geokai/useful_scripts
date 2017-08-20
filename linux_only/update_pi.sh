9efd3e1059f3cf3e0fced0ec2303e24262a2e735
George Kaimakis <geomatus@mac.com>
2017-08-20 12:45:21 +0100

    'update.sh' to update repos (the upgrade function not yet implemented):

---
+++ linux_only/update_pi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"""this script updates the raspbian repos and then upgrades the system"""
+
+# This file was created on 20/08/2017
+# Author: George Kaimakis - https://github.com/geokai
+sudo apt-get update -y
