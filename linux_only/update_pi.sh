abc4f8a09b9184c4694e0c10891c6ab5abcf0a1b
George Kaimakis <geomatus@mac.com>
2017-08-20 13:14:59 +0100

    Adds echo statements for improved visual feed-back:

--- linux_only/update_pi.sh
+++ linux_only/update_pi.sh
@@ -5,8 +5,24 @@
 # This file was created on 20/08/2017
 # Author: George Kaimakis - https://github.com/geokai
 
-echo 'updating the raspbian reopos...'
+clear
+echo
+echo '––––––––––––––––––––––––––––––––––––––'
+echo 'updating the raspbian repo listings...'
+echo '––––––––––––––––––––––––––––––––––––––'
+echo
 sudo apt-get update -y
-
+echo
+echo '–––––––––––––––––––––––––––'
+echo 'update complete.'
 echo 'now upgrading the system...'
+echo '–––––––––––––––––––––––––––'
+echo
 sudo apt-get upgrade -y
+echo
+echo '–––––––––––––––––'
+echo 'upgrade complete.'
+echo '–––––––––––––––––'
+echo
+sleep 5
+clear
