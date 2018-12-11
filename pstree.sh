1a91743232f4a82b24ba5398b349d8f160c2f2f8
George Kaimakis <geoptus@gmail.com>
2018-12-11 18:42:04 +0000

    Alter loop interval and interval selection range:

--- pstree.sh
+++ pstree.sh
@@ -5,20 +5,22 @@
 
 clear
 
-echo -e "select interval,  2 to 5 seconds: \c"
+echo -e "select interval,  2 to 10 seconds: \c"
 read INTERVAL
 
-if (( $INTERVAL > 1 )) && (( $INTERVAL < 6 ))
+if (( $INTERVAL > 1 )) && (( $INTERVAL < 11 ))
     then
         while :
              do 
                 clear
                 pstree -up
+                echo
+                echo "This process: $$"
                 sleep $INTERVAL
             done
     else
-        echo -e "select only from 2 to 5: \c"
+        echo -e "select only from 2 to 10: \c"
         sleep 2
         clear
-        exit
+        exit 1
 fi
